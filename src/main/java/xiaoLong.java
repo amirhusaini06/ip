1602e98a26554964e3bb60a25aea5606539f51c3
amirhusaini06 <63500859+amirhusaini06@users.noreply.github.com>
2025-01-23 10:31:10 +0800

    level 0

---
+++ src/main/java/xiaoLong.java
@@ -0,0 +1,10 @@
+public class xiaoLong {
+    public static void main(String[] args) {
+        System.out.println("___________________________________________________________________________________________________");
+        System.out.println("What's up? I'm xiaoLong, your trusty chatbot");
+        System.out.println("What can I do for you?");
+        System.out.println("___________________________________________________________________________________________________");
+        System.out.println("Sorry, I don't have any replies yet.\n" + "My creator hasn't programmed me to do anything. Bye for now!");
+
+    }
+}
