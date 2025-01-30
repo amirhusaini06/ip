5aff7f3c3c09b17e658b00f7da90c01e1c6cc635
amirhusaini06 <63500859+amirhusaini06@users.noreply.github.com>
2025-01-31 00:14:51 +0800

    level 3. created new class Task.java and chatbot now has the ability to mark and unmark tasks on the list

--- src/main/java/XiaoLong.java
+++ src/main/java/XiaoLong.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class XiaoLong {
     public static void main(String[] args) {
-        // Print the design
         String design =
                 " ___    ___ ___  ________  ________  ___       ________  ________   ________     \n" +
                         "|\\  \\  /  /|\\  \\|\\   __  \\|\\   __  \\|\\  \\     |\\   __  \\|\\   ___  \\|\\   ____\\    \n" +
@@ -21,7 +20,7 @@ public class XiaoLong {
 
         Scanner scanner = new Scanner(System.in);
         String input;
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while (true) {
@@ -30,27 +29,40 @@ public class XiaoLong {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Goodbye! Nice meeting you, so long from XiaoLong");
                 break;
-            } else if(input.equalsIgnoreCase("list")){
+
+            } else if (input.equalsIgnoreCase("list")) {
                 if (taskCount == 0) {
                     System.out.println("List is empty. Please put any other input to add something, or say bye to exit!");
+                } else {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i].toString());
+                    }
+                    System.out.println("Input mark/unmark N to mark/unmark the Nth item on your list");
                 }
-                for (int i = 0; i < taskCount; i+=1) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
-                }
+
+            } else if (input.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                tasks[taskNumber - 1].markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + tasks[taskNumber - 1].toString());
+
+            } else if (input.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                tasks[taskNumber - 1].markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:\n" + tasks[taskNumber - 1].toString());
 
             } else {
                 if (taskCount < 100) {
-                    tasks[taskCount] = input;
-                    taskCount += 1;
+                    tasks[taskCount] = new Task(input);
+                    taskCount++;
                     System.out.println("Added to list: " + input);
                     System.out.println("Say list to show list, or say bye to exit!");
                 } else {
                     System.out.println("Task list is full. Say list to show list, or say bye to exit!");
                 }
-
             }
-
         }
+
         scanner.close();
     }
 }
