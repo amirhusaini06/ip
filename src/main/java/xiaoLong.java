a6f309b68fb351cdd525ca706e4c6af0e9499c86
amirhusaini06 <63500859+amirhusaini06@users.noreply.github.com>
2025-01-30 23:01:58 +0800

    level 1, has a new echo feature and logo design

--- src/main/java/xiaoLong.java
+++ src/main/java/xiaoLong.java
@@ -1,10 +1,38 @@
-public class xiaoLong {
-    public static void main(String[] args) {
-        System.out.println("___________________________________________________________________________________________________");
-        System.out.println("What's up? I'm xiaoLong, your trusty chatbot");
-        System.out.println("What can I do for you?");
-        System.out.println("___________________________________________________________________________________________________");
-        System.out.println("Sorry, I don't have any replies yet.\n" + "My creator hasn't programmed me to do anything. Bye for now!");
+import java.util.Scanner;
 
+public class XiaoLong {
+    public static void main(String[] args) {
+        // Print the design
+        String design =
+                " ___    ___ ___  ________  ________  ___       ________  ________   ________     \n" +
+                        "|\\  \\  /  /|\\  \\|\\   __  \\|\\   __  \\|\\  \\     |\\   __  \\|\\   ___  \\|\\   ____\\    \n" +
+                        "\\ \\  \\/  / | \\  \\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\    \\ \\  \\|\\  \\ \\  \\\\ \\  \\ \\  \\___|    \n" +
+                        " \\ \\    / / \\ \\  \\ \\   __  \\ \\  \\\\  \\ \\  \\    \\ \\  \\\\  \\ \\  \\\\ \\  \\ \\  \\  ___  \n" +
+                        "  /     \\/   \\ \\  \\ \\  \\ \\  \\ \\  \\\\  \\ \\  \\____\\ \\  \\\\  \\ \\  \\\\ \\  \\ \\  \\|\\  \\ \n" +
+                        " /  /\\   \\    \\ \\__\\ \\__\\ \\__\\ \\_______\\ \\_______\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\\n" +
+                        "/__/ /\\ __\\    \\|__|\\|__|\\|__|\\|_______|\\|_______|\\|_______|\\|__| \\|__|\\|_______|\n" +
+                        "|__|/ \\|__|                                                                       ";
+
+        System.out.println("___________________________________________________________________________________________________");
+        System.out.println(design);  // Print the design
+        System.out.println("What's up? I'm XiaoLong, your trusty chatbot");
+        System.out.println("I can echo any input you make, give it a try!");
+        System.out.println("___________________________________________________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Goodbye! Nice meeting you, so long from XiaoLong");
+                break;
+            } else {
+                System.out.println(input);
+            }
+
+        }
+        scanner.close();
     }
 }
