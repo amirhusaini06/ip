5e8bbb47d17d65518b0ded875f57cda878b93a37
amirhusaini06 <63500859+amirhusaini06@users.noreply.github.com>
2025-02-06 11:29:38 +0800

    updated coding standard for both XiaoLong.java and Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,24 +1,49 @@
+/**
+ * Represents a task with a description and completion status.
+ */
 public class Task {
-    protected String description;
-    protected boolean isDone;
+    private String description;
+    private boolean isDone;
 
+    /**
+     * Constructs a Task with the specified description.
+     *
+     * @param description The task description.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon of the task.
+     *
+     * @return "X" if the task is done, otherwise a space.
+     */
     public String getStatusIcon() {
-        return (isDone ? "X" : " ");
+        return isDone ? "X" : " ";
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone() {
         isDone = true;
     }
 
+    /**
+     * Marks the task as not done.
+     */
     public void markAsNotDone() {
         isDone = false;
     }
 
+    /**
+     * Returns a string representation of the task.
+     *
+     * @return A string in the format "[statusIcon] description".
+     */
+    @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
     }
--- src/main/java/XiaoLong.java
+++ src/main/java/XiaoLong.java
@@ -1,68 +1,140 @@
 import java.util.Scanner;
 
+/**
+ * A chatbot program that manages a list of tasks.
+ */
 public class XiaoLong {
-    public static void main(String[] args) {
-        String design =
-                " ___    ___ ___  ________  ________  ___       ________  ________   ________     \n" +
-                        "|\\  \\  /  /|\\  \\|\\   __  \\|\\   __  \\|\\  \\     |\\   __  \\|\\   ___  \\|\\   ____\\    \n" +
-                        "\\ \\  \\/  / | \\  \\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\    \\ \\  \\|\\  \\ \\  \\\\ \\  \\ \\  \\___|    \n" +
-                        " \\ \\    / / \\ \\  \\ \\   __  \\ \\  \\\\  \\ \\  \\    \\ \\  \\\\  \\ \\  \\\\ \\  \\ \\  \\  ___  \n" +
-                        "  /     \\/   \\ \\  \\ \\  \\ \\  \\ \\  \\\\  \\ \\  \\____\\ \\  \\\\  \\ \\  \\\\ \\  \\ \\  \\|\\  \\ \n" +
-                        " /  /\\   \\    \\ \\__\\ \\__\\ \\__\\ \\_______\\ \\_______\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\\n" +
-                        "/__/ /\\ __\\    \\|__|\\|__|\\|__|\\|_______|\\|_______|\\|_______|\\|__| \\|__|\\|_______|\n" +
-                        "|__|/ \\|__|                                                                       ";
+    private static final int MAX_TASKS = 100;
+    private static final String DESIGN =
+            " ___    ___ ___  ________  ________  ___       ________  ________   ________     \n" +
+                    "|\\  \\  /  /|\\  \\|\\   __  \\|\\   __  \\|\\  \\     |\\   __  \\|\\   ___  \\|\\   ____\\    \n" +
+                    "\\ \\  \\/  / | \\  \\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\    \\ \\  \\|\\  \\ \\  \\\\ \\  \\ \\  \\___|    \n" +
+                    " \\ \\    / / \\ \\  \\ \\   __  \\ \\  \\\\  \\ \\  \\    \\ \\  \\\\  \\ \\  \\\\ \\  \\ \\  \\  ___  \n" +
+                    "  /     \\/   \\ \\  \\ \\  \\ \\  \\ \\  \\\\  \\ \\  \\____\\ \\  \\\\  \\ \\  \\\\ \\  \\ \\  \\|\\  \\ \n" +
+                    " /  /\\   \\    \\ \\__\\ \\__\\ \\__\\ \\_______\\ \\_______\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\\n" +
+                    "/__/ /\\ __\\    \\|__|\\|__|\\|__|\\|_______|\\|_______|\\|_______|\\|__| \\|__|\\|_______|\n" +
+                    "|__|/ \\|__|                                                                       ";
 
+    private Task[] tasks;
+    private int taskCount;
+
+    /**
+     * Constructs a XiaoLong chatbot instance.
+     */
+    public XiaoLong() {
+        tasks = new Task[MAX_TASKS];
+        taskCount = 0;
+    }
+
+    /**
+     * Starts the XiaoLong chatbot.
+     */
+    public void start() {
         System.out.println("___________________________________________________________________________________________________");
-        System.out.println(design);
+        System.out.println(DESIGN);
         System.out.println("What's up? I'm XiaoLong, your trusty chatbot");
         System.out.println("What can I do for you today? I can make a list for you. Just input anything. Say bye to exit!");
         System.out.println("___________________________________________________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
         String input;
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
 
         while (true) {
-            input = scanner.nextLine();
+            input = scanner.nextLine().trim();
 
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Goodbye! Nice meeting you, so long from XiaoLong");
                 break;
-
             } else if (input.equalsIgnoreCase("list")) {
-                if (taskCount == 0) {
-                    System.out.println("List is empty. Please put any other input to add something, or say bye to exit!");
-                } else {
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ". " + tasks[i].toString());
-                    }
-                    System.out.println("Input mark/unmark N to mark/unmark the Nth item on your list");
-                }
-
+                displayTasks();
             } else if (input.startsWith("mark")) {
-                int taskNumber = Integer.parseInt(input.split(" ")[1]);
-                tasks[taskNumber - 1].markAsDone();
-                System.out.println("Nice! I've marked this task as done:\n" + tasks[taskNumber - 1].toString());
-
+                handleMarkCommand(input);
             } else if (input.startsWith("unmark")) {
-                int taskNumber = Integer.parseInt(input.split(" ")[1]);
-                tasks[taskNumber - 1].markAsNotDone();
-                System.out.println("OK, I've marked this task as not done yet:\n" + tasks[taskNumber - 1].toString());
-
+                handleUnmarkCommand(input);
             } else {
-                if (taskCount < 100) {
-                    tasks[taskCount] = new Task(input);
-                    taskCount++;
-                    System.out.println("Added to list: " + input);
-                    System.out.println("Say list to show list, or say bye to exit!");
-                } else {
-                    System.out.println("Task list is full. Say list to show list, or say bye to exit!");
-                }
+                addTask(input);
             }
         }
 
         scanner.close();
+    }
+
+    /**
+     * Displays the list of tasks.
+     */
+    private void displayTasks() {
+        if (taskCount == 0) {
+            System.out.println("List is empty. Please put any other input to add something, or say bye to exit!");
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println((i + 1) + ". " + tasks[i]);
+            }
+            System.out.println("Input mark/unmark N to mark/unmark the Nth item on your list");
+        }
+    }
+
+    /**
+     * Handles the 'mark' command to mark a task as done.
+     *
+     * @param input The user input containing the 'mark' command and task number.
+     */
+    private void handleMarkCommand(String input) {
+        try {
+            int taskNumber = Integer.parseInt(input.split(" ")[1]);
+            if (taskNumber >= 1 && taskNumber <= taskCount) {
+                tasks[taskNumber - 1].markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + tasks[taskNumber - 1]);
+            } else {
+                System.out.println("Invalid task number. Please enter a valid number.");
+            }
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please enter a valid task number to mark.");
+        }
+    }
+
+    /**
+     * Handles the 'unmark' command to mark a task as not done.
+     *
+     * @param input The user input containing the 'unmark' command and task number.
+     */
+    private void handleUnmarkCommand(String input) {
+        try {
+            int taskNumber = Integer.parseInt(input.split(" ")[1]);
+            if (taskNumber >= 1 && taskNumber <= taskCount) {
+                tasks[taskNumber - 1].markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:\n" + tasks[taskNumber - 1]);
+            } else {
+                System.out.println("Invalid task number. Please enter a valid number.");
+            }
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please enter a valid task number to unmark.");
+        }
+    }
+
+    /**
+     * Adds a new task to the list.
+     *
+     * @param description The description of the task to add.
+     */
+    private void addTask(String description) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount] = new Task(description);
+            taskCount++;
+            System.out.println("Added to list: " + description);
+            System.out.println("Say list to show list, or say bye to exit!");
+        } else {
+            System.out.println("Task list is full. Say list to show list, or say bye to exit!");
+        }
+    }
+
+    /**
+     * The main method to run the XiaoLong chatbot.
+     *
+     * @param args Command-line arguments (not used).
+     */
+    public static void main(String[] args) {
+        XiaoLong xiaoLong = new XiaoLong();
+        xiaoLong.start();
     }
 }
